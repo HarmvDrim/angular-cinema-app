ccb27b18c273389f61ff1dfffad987a8fedffd5f
Harm van Drimmelen <34367387+HarmvDrim@users.noreply.github.com>
2019-11-15 16:49:03 +0100

    added use cases

--- src/app/about/usecases/usecases.component.ts
+++ src/app/about/usecases/usecases.component.ts
@@ -28,11 +28,39 @@ export class UsecasesComponent implements OnInit {
       id: 'UC-02',
       name: 'Details film bekijken',
       description: 'Hiermee kan een gebruiker details zien van een film',
-      scenario: ['Gebruiker klikt op films tap', '', 'Stap 3'],
+      scenario: ['Gebruiker klikt op films tap', 'Gebruiker klikt op een film'],
+      actor: this.PLAIN_USER,
+      precondition: 'De actor is ingelogd',
+      postcondition: 'De gebruiker ziet het detail scherm van de film'
+    },
+    {
+      id: 'UC-03',
+      name: 'Film toevoegen',
+      description: 'Hiermee voegt de admin een film toe',
+      scenario: ['Admin gaat naar film tap', 'Admin klikt op add movie tab', 'Admin voegt gegevens van de film toe en saved de film'],
       actor: this.ADMIN_USER,
       precondition: 'De actor is ingelogd',
-      postcondition: 'Het doel is bereikt.'
+      postcondition: 'Nieuwe film is toegevoegd'
+    },
+    {
+      id: 'UC-04',
+      name: 'Films bekijken',
+      description: 'Hiermee kan een gebruiker alle films zien',
+      scenario: ['Gebruiker klikt op films tap'],
+      actor: this.PLAIN_USER,
+      precondition: 'De actor is ingelogd',
+      postcondition: 'De gebruiker kan alle films zien'
+    },
+    {
+      id: 'UC-05',
+      name: 'Film voorstelling bekijken',
+      description: 'Hiermee kan een gebruiker alle films zien',
+      scenario: ['Gebruiker klikt op voorstellingen tap', 'Gebruiker klikt op een tijd van een film'],
+      actor: this.PLAIN_USER,
+      precondition: 'De actor is ingelogd',
+      postcondition: 'De gebruiker ziet de tijd en film van de voorstelling'
     }
+
   ]
 
   constructor() {}
